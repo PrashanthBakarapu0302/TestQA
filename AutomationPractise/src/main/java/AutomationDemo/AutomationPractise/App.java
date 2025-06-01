bcdfe2e85d2a88d048938933cf0f086c31d12027
PrashanthBakarapu <PrashanthBakarapu0302@gmail.com>
2025-06-01 13:29:12 +0530

    push

--- AutomationPractise/src/main/java/AutomationDemo/AutomationPractise/App.java
+++ AutomationPractise/src/main/java/AutomationDemo/AutomationPractise/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "hello automation" );
+        System.out.println( "hello automations uywue" );
         
     }
 }
